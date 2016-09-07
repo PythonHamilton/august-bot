2e4b0bfacb60477368ec5e97462ee5fdc110fec2
Colin Gorrie <colin@colingorrie.com>
2016-09-07 19:38:30 -0400

    Add txt file persistence

--- server.py
+++ server.py
@@ -5,9 +5,15 @@ from bot import Bot
 
 bot = Bot()
 
+with open('history.txt', 'r') as fh:
+    for line in fh:
+        bot.add_sentence(line.strip())
 
 token = "GET YOUR OWN TOKEN"  # found at https://api.slack.com/web#authentication
 sc = SlackClient(token)
+
+fh = open('history.txt', 'a')
+
 if sc.rtm_connect():
 
     while True:
@@ -23,8 +29,11 @@ if sc.rtm_connect():
                                 icon_emoji=":robot_face:")
                 else:
                     if "subtype" not in evt or ("subtype" in evt and evt["subtype"] != "bot_message"):
+                        fh.write(evt['text']+'\n')
                         bot.add_sentence(evt["text"])
 
         time.sleep(1)
 else:
     print("Connection Failed, invalid token?")
+
+fh.close()
